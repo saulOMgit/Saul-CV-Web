7e286f7f16fefb0555d8667d1881f2af2ecf57da
saulOMgit <sauloteromelchor@gmail.com>
2025-01-28 12:47:57 +0100

    netlify config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,11 @@ import compress from "astro-compress";
 import icon from "astro-icon";
 import { defineConfig } from 'astro/config';
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/netlify/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon(), compress()],
   output: "server",
-  adapter: vercel()
+  adapter: netlify()
 });
